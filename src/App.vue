828fb59665193f12d79e1d2fe53adf74be4db525
kromate <akpananthony33@gmail.com>
2022-09-29 17:38:37 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-	<main class="px-8">
-		<h1 class="text-4xl font-bold">Hello</h1>
+	<main class="px-8 text-white">
+		<h1 class="text-7xl font-semibold">Hello</h1>
+    <p class="text-3xl font-medium">KROMTECH has been shutdown and is no longer active</p>
 	</main>
 </template>
 
@@ -9,15 +10,20 @@ export default {};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap');
+
 *{
   padding: 0;
   margin: 0;
+  font-family: 'DM Sans', sans-serif;
+
 }
 main, #app {
 	height: 100vh;
 	width: 100vw;
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
+  flex-direction: column;
   align-items: center;
 }
 

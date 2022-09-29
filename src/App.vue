0d50ae5bdbad6351cb5f0d191b7223ce54c9f97e
kromate <akpananthony33@gmail.com>
2022-09-29 17:54:11 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
 	<main class="px-8 text-white">
-		<h1 class="text-7xl font-semibold">Hello</h1>
+		<h1 class="text-6xl font-semibold mb-4">Hello there 👋🏾</h1>
     <p class="text-3xl font-medium">KROMTECH has been shutdown and is no longer active</p>
+    <div class="flex gap-4 mt-9">
+      <a href="https://kaa.kromtech.io/" class="bg-[#0000005f] hover:bg-black px-10 py-2 rounded-lg shadow">KAA</a>
+      <a href="https://studee.kromtech.io/" class="bg-[#0000005f] hover:bg-black px-10 py-2 rounded-lg shadow">Studee</a>
+    </div>
 	</main>
 </template>
 
